27350673219237a0d14cd164591cbbb9729542f0
djxmax <maxime.lucquin@gmail.com>
2015-06-25 16:23:12 +0200

    ajout d'info

--- parking_ferme.php
+++ parking_ferme.php
@@ -11,8 +11,14 @@ function get_close_parking_list($db)
 
 	while($donnees = mysqli_fetch_assoc($req))
 	{
-		$array_temp=array("id"=>$donnees['id'], "latitude"=>$donnees['latitude'], "longitude"=>$donnees['longitude'], "places_total"=>$donnees['places_total'], 
-		"places_restantes"=>$donnees['places_restantes'], "adresse"=>utf8_encode($donnees['adresse']), "code_postal"=>$donnees['code_postal'], "ville"=>utf8_encode($donnees['ville']));
+		$array_temp=array("id"=>$donnees['id'], "nom"=>utf8_encode($donnees['nom']), "latitude"=>$donnees['latitude'], 
+		"longitude"=>$donnees['longitude'], "places_total"=>$donnees['places_total'], 
+		"places_restantes"=>$donnees['places_restantes'], "adresse"=>utf8_encode($donnees['adresse']), 
+		"code_postal"=>$donnees['code_postal'], "ville"=>utf8_encode($donnees['ville']), 
+		"carte_bancaire"=>$donnees['carte_bancaire'], "billets"=>$donnees['billets'],
+		"pieces"=>$donnees['pieces'], "prive"=>$donnees['prive'],
+		"horaire_payant_semaine"=>utf8_encode($donnees['horaire_payant_semaine']),
+		"horaire_payant_dimanche"=>utf8_encode($donnees['horaire_payant_dimanche']));
 		
 		array_push($close_parking_list, $array_temp);
 	}
